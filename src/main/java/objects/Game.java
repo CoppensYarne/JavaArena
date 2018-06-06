a458e498da4bb2e6043d5fffe36f3e3161266cf6
Coppens Yarne <yarne.coppens@student.howest.be>
2018-06-06 23:19:42 +0200

    Fixed bug where characters would not go to the right of the arena

--- src/main/java/objects/Game.java
+++ src/main/java/objects/Game.java
@@ -50,12 +50,12 @@ public class Game {
             allWeapons = wr.getAllWeapons();
             cr = new CharacterRepository();
             allCharacters = cr.getAllCharacters();
-            if((arenaLength * arenaHeight) < (allCharacters.size() + allWeapons.size())){
-                throw new ArenaTooSmallException("User " + gameOwner + " requested a size of " + arenaLength + "x" + arenaHeight + "=" + (arenaLength * arenaHeight+
-                " but it needs to be at least " + ((allCharacters.size() + allWeapons.size()))));
+            if ((arenaLength * arenaHeight) < (allCharacters.size() + allWeapons.size())) {
+                throw new ArenaTooSmallException("User " + gameOwner + " requested a size of " + arenaLength + "x" + arenaHeight + "=" + (arenaLength * arenaHeight +
+                        " but it needs to be at least " + ((allCharacters.size() + allWeapons.size()))));
             }
             startGame(arenaLength, arenaHeight);
-        }catch(ArenaTooSmallException ex){
+        } catch (ArenaTooSmallException ex) {
             errorMessage = ex.getMessage();
         }
     }
@@ -132,141 +132,143 @@ public class Game {
                     }
                 }
 
-                if (!(newPosition < 0) && !(newPosition > arena.getSquares()) && !(positionTaken) && (!(newPosition % arena.getLength() == 0)||arena.getLength() == 1)) {
-                    character.setPosition(newPosition);
+                if (!(newPosition < 0) && !(newPosition >= arena.getSquares()) && !(positionTaken)) {
+                    if (!(character.getPosition() % arena.getLength() == 0 && newPosition - 1 == character.getPosition())) {
+                        character.setPosition(newPosition);
+                    }
                 }
             }
         }
     }
 
 
-        public String attackAdjacentCharacters () {
-            String toReturnString = "";
-            for (ArenaCharacter arenaCharacter : allCharacters) {
-                ArrayList<Integer> allPossiblePositions = new ArrayList<Integer>();
+    public String attackAdjacentCharacters() {
+        String toReturnString = "";
+        for (ArenaCharacter arenaCharacter : allCharacters) {
+            ArrayList<Integer> allPossiblePositions = new ArrayList<Integer>();
 
-                for(int x = 1; x <= arenaCharacter.getCurrentWeapon().getRange(); x++){
-                    allPossiblePositions.add(arenaCharacter.getPosition() + x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() - x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() + arena.getLength() * x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() - arena.getLength() * x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() + arena.getLength()*x + x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() + arena.getLength()*x - x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() - arena.getLength()*x + x);
-                    allPossiblePositions.add(arenaCharacter.getPosition() - arena.getLength()*x - x);
-                }
+            for (int x = 1; x <= arenaCharacter.getCurrentWeapon().getRange(); x++) {
+                allPossiblePositions.add(arenaCharacter.getPosition() + x);
+                allPossiblePositions.add(arenaCharacter.getPosition() - x);
+                allPossiblePositions.add(arenaCharacter.getPosition() + arena.getLength() * x);
+                allPossiblePositions.add(arenaCharacter.getPosition() - arena.getLength() * x);
+                allPossiblePositions.add(arenaCharacter.getPosition() + arena.getLength() * x + x);
+                allPossiblePositions.add(arenaCharacter.getPosition() + arena.getLength() * x - x);
+                allPossiblePositions.add(arenaCharacter.getPosition() - arena.getLength() * x + x);
+                allPossiblePositions.add(arenaCharacter.getPosition() - arena.getLength() * x - x);
+            }
 
-                ArrayList<Integer> allAdjacentFilledPositions = new ArrayList<>();
-                for (Integer position : allPossiblePositions) {
-                    if (arena.isThereCharacterAtPosition(position)) {
-                        allAdjacentFilledPositions.add(position);
-                    }
-                }
-                if (allAdjacentFilledPositions.size() != 0) {
-                    Random random = new Random();
-                    int randomTargetPosition = allAdjacentFilledPositions.get(random.nextInt(allAdjacentFilledPositions.size()));
-                    ArenaCharacter defendingCharacter = arena.acquireCharacterAtPosition(randomTargetPosition);
-                    Limb targetedLimb = defendingCharacter.acquireRandomLimb();
-                    toReturnString += arenaCharacter.attack(defendingCharacter, targetedLimb);
+            ArrayList<Integer> allAdjacentFilledPositions = new ArrayList<>();
+            for (Integer position : allPossiblePositions) {
+                if (arena.isThereCharacterAtPosition(position)) {
+                    allAdjacentFilledPositions.add(position);
                 }
             }
-            return toReturnString;
+            if (allAdjacentFilledPositions.size() != 0) {
+                Random random = new Random();
+                int randomTargetPosition = allAdjacentFilledPositions.get(random.nextInt(allAdjacentFilledPositions.size()));
+                ArenaCharacter defendingCharacter = arena.acquireCharacterAtPosition(randomTargetPosition);
+                Limb targetedLimb = defendingCharacter.acquireRandomLimb();
+                toReturnString += arenaCharacter.attack(defendingCharacter, targetedLimb);
+            }
+        }
+        return toReturnString;
+    }
+
+
+    public String nextTurn() {
+        String toReturnString = "";
+        ArrayList<ArenaCharacter> charactersToAttack = new ArrayList<>(cr.getAllCharacters());
+        Collections.shuffle(charactersToAttack);
+
+        moveCharactersRandom();
+
+
+        for (ArenaCharacter character : allCharacters) {
+            for (Weapon weapon : allWeapons) {
+                if (weapon.getPosition() == character.getPosition()) {
+                    toReturnString += character.equipWeapon(weapon);
+                    weapon.setPosition(arena.getSquares() + 1);
+                }
+            }
         }
 
 
-        public String nextTurn () {
-            String toReturnString = "";
-            ArrayList<ArenaCharacter> charactersToAttack = new ArrayList<>(cr.getAllCharacters());
-            Collections.shuffle(charactersToAttack);
+        toReturnString += attackAdjacentCharacters();
 
-            moveCharactersRandom();
+        toReturnString += "\n";
 
-
-            for (ArenaCharacter character : allCharacters) {
-                for (Weapon weapon : allWeapons) {
-                    if (weapon.getPosition() == character.getPosition()) {
-                        toReturnString += character.equipWeapon(weapon);
-                        weapon.setPosition(arena.getSquares() + 1);
-                    }
-                }
-            }
-
-
-            toReturnString += attackAdjacentCharacters();
-
+        ArrayList<ArenaCharacter> deadCharacters = acquireNewDeadCharacters();
+        if (deadCharacters.size() != 0) {
+            toReturnString += deadCharacters;
             toReturnString += "\n";
-
-            ArrayList<ArenaCharacter> deadCharacters = acquireNewDeadCharacters();
-            if (deadCharacters.size() != 0) {
-                toReturnString += deadCharacters;
-                toReturnString += "\n";
-            }
-
-            for (ArenaCharacter deadCharacter : deadCharacters) {
-                toReturnString += deadCharacter.getName() + " died.";
-                toReturnString += "\n";
-                int characterPosition = deadCharacter.getPosition();
-                if (!deadCharacter.getCurrentWeapon().getName().equals("Fists")) {
-                    deadCharacter.getCurrentWeapon().setPosition(characterPosition);
-                }
-                allCharacters.remove(deadCharacter);
-            }
-
-            updateArena();
-
-            //toReturnString = toReturnString.replace(".", ".\n\n");
-
-            if(allCharacters.size() == 1){
-                gameOver = true;
-            }
-
-            gameEvents = toReturnString;
-            return toReturnString;
         }
 
-        public ArrayList<ArenaCharacter> acquireNewDeadCharacters() {
-            ArrayList<ArenaCharacter> newDeadCharacters = new ArrayList<>();
-
-            for (ArenaCharacter character : allCharacters) {
-                if (character.getCurrentHealth() <= 0) {
-                    newDeadCharacters.add(character);
-                    graveyard.add(character);
-                }
+        for (ArenaCharacter deadCharacter : deadCharacters) {
+            toReturnString += deadCharacter.getName() + " died.";
+            toReturnString += "\n";
+            int characterPosition = deadCharacter.getPosition();
+            if (!deadCharacter.getCurrentWeapon().getName().equals("Fists")) {
+                deadCharacter.getCurrentWeapon().setPosition(characterPosition);
             }
-
-            return newDeadCharacters;
+            allCharacters.remove(deadCharacter);
         }
 
-        public Weapon acquireRandomWeapon() {
-            Random randomGen = new Random();
-            int index = randomGen.nextInt(allWeapons.size());
-            return allWeapons.get(index);
+        updateArena();
+
+        //toReturnString = toReturnString.replace(".", ".\n\n");
+
+        if (allCharacters.size() == 1) {
+            gameOver = true;
         }
 
-        public void updateArena () {
-            ArrayList<ArenaObject> allObjects = new ArrayList<>();
+        gameEvents = toReturnString;
+        return toReturnString;
+    }
 
-            for (ArenaObject weapon : allWeapons) {
-                allObjects.add(weapon);
-            }
+    public ArrayList<ArenaCharacter> acquireNewDeadCharacters() {
+        ArrayList<ArenaCharacter> newDeadCharacters = new ArrayList<>();
 
-            for (ArenaObject character : allCharacters) {
-                allObjects.add(character);
-            }
-
-            arena.setArenaObjects(allObjects);
-
-        }
-
-        private boolean acquireRandomChance ( int chancePercent){
-            Random randomGen = new Random();
-            int randomInt = randomGen.nextInt(100);
-            if (randomInt < chancePercent) {
-                return true;
-            } else {
-                return false;
+        for (ArenaCharacter character : allCharacters) {
+            if (character.getCurrentHealth() <= 0) {
+                newDeadCharacters.add(character);
+                graveyard.add(character);
             }
         }
+
+        return newDeadCharacters;
+    }
+
+    public Weapon acquireRandomWeapon() {
+        Random randomGen = new Random();
+        int index = randomGen.nextInt(allWeapons.size());
+        return allWeapons.get(index);
+    }
+
+    public void updateArena() {
+        ArrayList<ArenaObject> allObjects = new ArrayList<>();
+
+        for (ArenaObject weapon : allWeapons) {
+            allObjects.add(weapon);
+        }
+
+        for (ArenaObject character : allCharacters) {
+            allObjects.add(character);
+        }
+
+        arena.setArenaObjects(allObjects);
+
+    }
+
+    private boolean acquireRandomChance(int chancePercent) {
+        Random randomGen = new Random();
+        int randomInt = randomGen.nextInt(100);
+        if (randomInt < chancePercent) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public boolean isGameOver() {
         return gameOver;
@@ -276,67 +278,67 @@ public class Game {
         this.gameOver = gameOver;
     }
 
-    public ArrayList<ArenaCharacter> getAllCharacters () {
-            return allCharacters;
-        }
-
-        public void setAllCharacters (ArrayList < ArenaCharacter > allCharacters) {
-            this.allCharacters = allCharacters;
-        }
-
-        public ArrayList<Weapon> getAllWeapons () {
-            return allWeapons;
-        }
-
-        public void setAllWeapons (ArrayList < Weapon > allWeapons) {
-            this.allWeapons = allWeapons;
-        }
-
-        public Arena generateArena () {
-            ArrayList<ArenaCharacter> allCharacters = this.allCharacters;
-            ArrayList<Weapon> allWeapons = this.allWeapons;
-
-            ArrayList<ArenaObject> allObjects = new ArrayList<>();
-
-            for (ArenaObject aObject : allCharacters) {
-                allObjects.add(aObject);
-            }
-
-            for (ArenaObject aObject : allWeapons) {
-                allObjects.add(aObject);
-            }
-
-            Arena arena = new Arena("TestArena", 10, 10, allObjects, Types.DESERT);
-            return arena;
-        }
-
-        public Arena generateArena ( int length, int height){
-            ArrayList<ArenaCharacter> allCharacters = this.allCharacters;
-            ArrayList<Weapon> allWeapons = this.allWeapons;
-
-            ArrayList<ArenaObject> allObjects = new ArrayList<>();
-
-            for (ArenaObject aObject : allCharacters) {
-                allObjects.add(aObject);
-            }
-
-            for (ArenaObject aObject : allWeapons) {
-                allObjects.add(aObject);
-            }
-
-            Arena arena = new Arena("TestArena", length, height, allObjects, Types.DESERT);
-            return arena;
-        }
-
-        public Arena getArena () {
-            return arena;
-        }
-
-        public JSONObject toJSON () throws JsonProcessingException {
-            ObjectMapper mapper = new ObjectMapper();
-            JSONObject jsonObject = new JSONObject(mapper.writeValueAsString(this));
-            return jsonObject;
-        }
-
-
+    public ArrayList<ArenaCharacter> getAllCharacters() {
+        return allCharacters;
     }
+
+    public void setAllCharacters(ArrayList<ArenaCharacter> allCharacters) {
+        this.allCharacters = allCharacters;
+    }
+
+    public ArrayList<Weapon> getAllWeapons() {
+        return allWeapons;
+    }
+
+    public void setAllWeapons(ArrayList<Weapon> allWeapons) {
+        this.allWeapons = allWeapons;
+    }
+
+    public Arena generateArena() {
+        ArrayList<ArenaCharacter> allCharacters = this.allCharacters;
+        ArrayList<Weapon> allWeapons = this.allWeapons;
+
+        ArrayList<ArenaObject> allObjects = new ArrayList<>();
+
+        for (ArenaObject aObject : allCharacters) {
+            allObjects.add(aObject);
+        }
+
+        for (ArenaObject aObject : allWeapons) {
+            allObjects.add(aObject);
+        }
+
+        Arena arena = new Arena("TestArena", 10, 10, allObjects, Types.DESERT);
+        return arena;
+    }
+
+    public Arena generateArena(int length, int height) {
+        ArrayList<ArenaCharacter> allCharacters = this.allCharacters;
+        ArrayList<Weapon> allWeapons = this.allWeapons;
+
+        ArrayList<ArenaObject> allObjects = new ArrayList<>();
+
+        for (ArenaObject aObject : allCharacters) {
+            allObjects.add(aObject);
+        }
+
+        for (ArenaObject aObject : allWeapons) {
+            allObjects.add(aObject);
+        }
+
+        Arena arena = new Arena("TestArena", length, height, allObjects, Types.DESERT);
+        return arena;
+    }
+
+    public Arena getArena() {
+        return arena;
+    }
+
+    public JSONObject toJSON() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JSONObject jsonObject = new JSONObject(mapper.writeValueAsString(this));
+        return jsonObject;
+    }
+
+
+}
